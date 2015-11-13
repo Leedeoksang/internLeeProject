db1cae99489db3051f56faa5c74825d811c7a0e7
Leedeoksang <duksang4834@encoredtech.com>
2015-11-13 19:24:57 +0900

    3d

--- client/app/services/graphic/graphic.service.js
+++ client/app/services/graphic/graphic.service.js
@@ -3,35 +3,82 @@
 angular.module('homeDashboardApp')
 	.service('graphic', function () {
     	// AngularJS will instantiate a singleton by calling "new" on this function
-  		this.prototype = {
-  			initWebGL: function (canvas) {
-  				var gl = null;
+  		this.initWebGL = function (canvas) {
+			var gl = null;
 
-  				try {
-  					gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-  				}
-  				catch(e) {
+			try {
+				gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+			}
+			catch(e) {
 
-  				}
+			}
 
-  				if (!gl) {
-  					alert('WebGL을 호환하지 않는 브라우저입니다.');
-  					gl = null;
-  				}
+			if (!gl) {
+				alert('WebGL을 호환하지 않는 브라우저입니다.');
+				gl = null;
+			}
 
-  				return gl;
-  			},
-  			initShaders: function (gl) {
-  				var fragmentShader = getShader(gl, 'shader-fs'),
-  					vertexShader = getShader(gl, 'shader-vs'),
-  					shaderProgram;
+			return gl;
+		};
 
-  				shaderProgram = gl.createProgram();
-  				gl.attachShader(shaderProgram, vertexShader);
-  				gl.attachShader(shaderProgram, fragmentShader);
-  				gl.linkProgram(shaderProgram);
+  		this.initShaders = function (gl) {
+			var fragmentShader = getShader(gl, 'shader-fs'),
+				vertexShader = getShader(gl, 'shader-vs'),
+				shaderProgram;
 
-  				
-  			}
-  		};
+			shaderProgram = gl.createProgram();
+			gl.attachShader(shaderProgram, vertexShader);
+			gl.attachShader(shaderProgram, fragmentShader);
+			gl.linkProgram(shaderProgram);
+
+			if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+				alert('shader program을 시작할 수 없습니다.');	
+			}
+
+			gl.useProgram(shaderProgram);
+
+			vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+			gl.enableVertexAttribArray(vertexPositionAttribute);
+		};
+
+		this.getShader = function (gl, id) {
+			var shaderScript,
+				theSource,
+				currentChild,
+				shader;
+
+			shaderScript = document.getElementById(id);
+
+			if (!shaderScript) {
+				return null;
+			}
+
+			theSource = '';
+			currentChild = shaderScript.firstChild;
+
+			while (currentChild) {
+				if (currentChild.nodeType == currentChild.TEXT_NODE) {
+					theSource += currentChild.textContent;
+				}
+				currentChild = currentChild.nextSibling;
+			}
+
+			if (shaderScript.type == 'x-shader/x-fragment') {
+				shader = gl.createShader(gl.FRAGMENT_SHADER);
+			} else if (shaderScript.type == 'x-shader/x-vertex') {
+				shader = gl.createShader(gl.VERTEX_SHADER);
+			} else {
+				return null;
+			}
+
+			gl.shaderSource(shader, theSource);
+			gl.compileShader(shader);
+			if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+				alert('shader를 컴파일하는데 실패하였습니다: ' + gl.getShaderInfoLog(shader));
+				return null;
+			}
+
+			return shader;
+		};
+		
   	});
--- client/app/views/main/main.controller.js
+++ client/app/views/main/main.controller.js
@@ -2,110 +2,126 @@
 
 angular.module('homeDashboardApp')
   .controller('MainCtrl', function ($scope, $window) {
-    var gl;
+    var scene,
+      camera,
+      renderer,
+      light,
+      sphere,
+      plane,
+      planeGeometry,
+      planeMaterial,
+      planeLine,
+      planeLineGeometry,
+      planeLineMaterial,
+      loader,
+      ambient;
 
     $scope.height = $window.innerHeight + 'px';
 
+    function init () {
+      var target = document.getElementById('3d-canvas');
+
+      scene = new THREE.Scene();
+      //.fog = new THREE.Fog(0x000000, 1, 500);
+
+      camera = new THREE.PerspectiveCamera(45, $window.innerWidth / $window.innerHeight, 1, 3500);
+      camera.position.z = 100;
+
+      ambient = new THREE.AmbientLight( 0x101030 );
+      scene.add( ambient );
+
+      //light = new THREE.PointLight( 0xffffff, 1.5, 1000 );
+      //light.position.set( 0, 100, 2 );
+      //scene.add( light );
+
+      loader = new THREE.JSONLoader();
+      loader.load('../../assets/3dmodels/car.js', function (geometry) {
+        /*var material = new THREE.Material(),
+          mesh = new THREE.Mesh(geometry, material);
+
+        console.log(geometry);
+        scene.add(mesh);*/
+      });
+
+      planeGeometry = new THREE.PlaneGeometry(120, 500);
+      planeMaterial = new THREE.MeshPhongMaterial({color: 0x878787, side: THREE.DoubleSide});
+      plane = new THREE.Mesh(planeGeometry, planeMaterial);
+      console.log(planeGeometry);
+      /*
+      planeLineGeometry = new THREE.PlaneGeometry(1, 500);
+      planeLineMaterial = new THREE.MeshPhongMaterial({color: 0xffffff, side: THREE.DoubleSide});
+      planeLine = new THREE.Mesh(planeLineGeometry, planeLineMaterial);
+
+      scene.add(plane);
+      scene.add(planeLine);
+      
+      camera.position.z = 100;
+
+      plane.rotation.x = -20.3;
+      
+      planeLine.rotation.x = -20.3;
+      planeLine.position.y += 0.1;
+*/
+      renderer = new THREE.WebGLRenderer();
+      renderer.setPixelRatio( $window.devicePixelRatio );
+      renderer.setSize($window.innerWidth, $window.innerHeight);
+      target.appendChild(renderer.domElement);
+
+      renderer.render( scene, camera );
+    }
+    init();
+    /*
     $scope.init = function () {
-      var canvas = document.getElementById('glcanvas');
+      var target = document.getElementById('3d-canvas');
 
-      gl = initWebGL(canvas);
+      //scene
+      scene = new THREE.Scene();
+      scene.fog = new THREE.Fog( 0xffffff, 10, 300);
 
-      if (gl) {
-        // Set clear color to black, fully opaque
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        // Enable depth testing
-        gl.enable(gl.DEPTH_TEST);
-        // Near things obscure far things
-        gl.depthFunc(gl.LEQUAL);
-        // Clear the color as well as the depth buffer.
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-      }
-    };
+      //light
 
-    function initWebGL (canvas) {
-      gl = null;
-  
-      try {
-        // Try to grab the standard context. If it fails, fallback to experimental.
-        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-      }
-      catch(e) {}
+      light = new THREE.PointLight( 0xff0000, 10, 200 );
+      light.position.set(50,50,50);
+      scene.add( light );
+
+      //camer
+      camera = new THREE.PerspectiveCamera( 75, $window.innerWidth / $window.innerHeight, 1, 3500 );
+
+      boxGeometry = new THREE.BoxGeometry( 100, 100, 100 );
+      boxMaterial = new THREE.MeshBasicMaterial({color: 0x555555});
+
+      cube = new THREE.Mesh( boxGeometry, boxMaterial );
       
-      // If we don't have a GL context, give up now
-      if (!gl) {
-        alert("Unable to initialize WebGL. Your browser may not support it.");
-        gl = null;
-      }
+      scene.add( cube );
       
-      return gl;
+      camera.position.z = 200;
+
+      renderer = new THREE.WebGLRenderer();
+      renderer.setPixelRatio( $window.devicePixelRatio );
+      renderer.setSize($window.innerWidth, $window.innerHeight);
+      target.appendChild(renderer.domElement);
+
+      document.addEventListener( 'resize', onWindowResize, false);
     }
 
-    function initShaders () {
-      var fragmentShader = getShader(gl, "shader-fs"),
-        vertexShader = getShader(gl, "shader-vs");
-      
-      // Create the shader program
-      
-      shaderProgram = gl.createProgram();
-      gl.attachShader(shaderProgram, vertexShader);
-      gl.attachShader(shaderProgram, fragmentShader);
-      gl.linkProgram(shaderProgram);
-      
-      // If creating the shader program failed, alert
-      
-      if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        alert("Unable to initialize the shader program.");
-      }
-      
-      gl.useProgram(shaderProgram);
-      
-      vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-      gl.enableVertexAttribArray(vertexPositionAttribute);
+    function onWindowResize () {
+      camera.aspect = $window.innerWidth / $window.innerHeight;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize( $window.innerWidth, $window.innerHeight );
     }
 
-    function getShader (gl, id) {
-      var shaderScript, theSource, currentChild, shader;
-  
-      shaderScript = document.getElementById(id);
-      
-      if (!shaderScript) {
-        return null;
-      }
-      
-      theSource = "";
-      currentChild = shaderScript.firstChild;
-      
-      while(currentChild) {
-        if (currentChild.nodeType == currentChild.TEXT_NODE) {
-          theSource += currentChild.textContent;
-        }
-        
-        currentChild = currentChild.nextSibling;
-      }
+    function animate () {
+      requestAnimationFrame( animate );
 
-      if (shaderScript.type == "x-shader/x-fragment") {
-        shader = gl.createShader(gl.FRAGMENT_SHADER);
-      } else if (shaderScript.type == "x-shader/x-vertex") {
-        shader = gl.createShader(gl.VERTEX_SHADER);
-      } else {
-         // Unknown shader type
-         return null;
-      }
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      cube.rotation.z += 0.01;
 
-      gl.shaderSource(shader, theSource);
-    
-      // Compile the shader program
-      gl.compileShader(shader);  
-        
-      // See if it compiled successfully
-      if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {  
-          alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));  
-          return null;  
-      }
-        
-      return shader;
+      renderer.render( scene, camera );
     }
 
     $scope.init();
+    animate();
+    */
 	});	
